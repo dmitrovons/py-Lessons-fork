22ba16f21649eaad35c1525f039ae3839766ab7c
VladVons <vladvons@gmail.com>
2022-01-09 23:42:18 +0200

    MyCommit

--- simple/http_server.py
+++ simple/http_server.py
@@ -3,7 +3,9 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def Parse(self, aPath: str) -> dict:
+    CntLoad = 0
+
+    def ParseQuery(self, aPath: str) -> dict:
         Query = urlparse(aPath).query
         if (Query == ''):
             Res = {}
@@ -25,10 +27,12 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-        Query = self.Parse(self.path)
-        DigitStr = self.GetDigitStr(Query.get('num', '0'))
-        BgColor = Query.get('color', 'white')
-        RevStr = Query.get('rev', 'reverse')
+        Query = self.ParseQuery(self.path)
+        ParaNum = self.GetDigitStr(Query.get('num', '0'))
+        ParaColor = Query.get('color', 'white')
+        ParaRever = Query.get('rever', 'Reverse string')
+
+        #self.CntLoad =+ 1
 
         Page = """
         <html>
@@ -43,10 +47,11 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             </head>
             <body>
                 <h1>Color: %s</h1><br>
-                <h1>Digit: %s</h1><br>
+                <h1>Number: %s</h1><br>
                 <h1>Reverse: %s</h1><br>
+                <h1>Loads: %s</h1><br>
             </body>
-        </html>""" % (BgColor, BgColor, DigitStr, RevStr[::-1])
+        </html>""" % (ParaColor, ParaColor, ParaNum, ParaRever[::-1], self.CntLoad)
 
         self.wfile.write(Page.encode())
 
